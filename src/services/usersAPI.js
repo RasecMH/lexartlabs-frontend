4ae56e17b7ffb2415f6d5059881f2b964483c871
Cesar Holanda <rasecmh0@gmail.com>
2024-03-07 05:34:45 -0300

    feat: add axios config

--- src/services/usersAPI.js
+++ src/services/usersAPI.js
@@ -8,6 +8,7 @@ export const loginRequest = async ({email, password}) => {
     return axios({
       method: "post",
       url: "https://lexartlabs-backend.vercel.app/users/login",
+      withCredentials: true,
       data: {
         email,
         password
@@ -19,6 +20,7 @@ export const registerRequest = async ({email, password, name}) => {
   return axios({
       method: "post",
       url: "https://lexartlabs-backend.vercel.app/users/register",
+      withCredentials: true,
       data: {
         email,
         password,
@@ -32,6 +34,7 @@ export const validateRequest = async () => {
   return axios({
       method: "get",
       url: "https://lexartlabs-backend.vercel.app/users/validate",
+      withCredentials: true,
       headers: {
         Authorization: token
       }
