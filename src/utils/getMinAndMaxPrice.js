cf012f9c510f6a29a93c3122e161ae0857d8d45b
RasecMH <rasecmh0@gmail.com>
2024-07-21 16:45:23 -0300

    fix: allprices map

--- src/utils/getMinAndMaxPrice.js
+++ src/utils/getMinAndMaxPrice.js
@@ -1,8 +1,8 @@
 export const getMinAndMaxPrice = (products) => {
-  const allPrices = products.map((product) => product.price);
+  const allPrices = products.map((product) => parseFloat(product.price));
   let minPrice = 0;
   let maxPrice = 0;
-  if (allPrices.length) {
+  if (allPrices.length > 0) {
     minPrice = Math.min(...allPrices);
     maxPrice = Math.max(...allPrices);
   }
